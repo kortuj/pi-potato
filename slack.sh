7ba5ab797de7be46fd6508c8f40c9241e407ab79
kortuj <kortuj@gmail.com>
2018-09-30 00:11:36 +0200

    implementing slack.sh

--- slack.sh
+++ slack.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+if [[ -z "$SLACK_RAJCATA" ]]; then
+    exit 0
+fi
+
+payload="{'text': '$1', 'channel': '#rajcata'}"
+curl -X POST --data-urlencode payload="$payload" "$SLACK_RAJCATA"
